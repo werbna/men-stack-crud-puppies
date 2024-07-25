8ffd1d4f6c266e93a8f755ad6859420430993000
werbnah <andrewchau1993@gmail.com>
2024-07-25 15:56:25 -0700

    minor corrections

--- server.js
+++ server.js
@@ -17,10 +17,25 @@ app.listen(3000, () => {
 
 //Query Request 
 
+
+// GET Index Page
 app.get("/", (req,res) => {
   res.render('index.ejs');
 })
 
+
+// GET Puppies New
 app.get('/puppies/new', (req,res) => {
   res.render('puppies/new.ejs');
 });
+
+//Post puppies/new
+app.post('/puppies', async (req,res) => {
+  // if (req.body.isGoodWithChildren === "on") {
+  //   req.body.isGoodWithChildren = true;
+  // } else {
+  //   req.body.isGoodWithChildren = false;
+  // }
+  await Puppy.create(req.body);
+  res.redirect('/puppies/new')
+})
