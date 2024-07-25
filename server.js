5fc27c49f609f93286a0c76bb390f5b500a160c0
werbnah <andrewchau1993@gmail.com>
2024-07-25 14:32:18 -0700

    made index.ejs

--- server.js
+++ server.js
@@ -3,3 +3,12 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config ();
 
+mongoose.connect(proncess.env.MONGODB_URI);
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
+
