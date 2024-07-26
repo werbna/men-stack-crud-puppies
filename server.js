12403d3a4b41bc87aaf03e6fb6e7584fbe65cb35
werbnah <andrewchau1993@gmail.com>
2024-07-26 14:36:00 -0700

    added controller folders, corrected link path, added show page

--- server.js
+++ server.js
@@ -2,19 +2,22 @@ const express = require("express");
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config ();
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+// const path = require('path');
+const app = express();
 
 const Puppy = require('./models/puppies.js');
 
-const app = express();
-
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use(morgan('dev'));
+// app.use(express.static(path.join(_dirname, 'public')));
 
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
-
-
 
 //Query Request 
 
@@ -24,20 +27,27 @@ app.get("/", (req,res) => {
   res.render('index.ejs');
 })
 
-// GET ALL puppies list
+//! Index Page Above this.
+
+//GET Puppies New
+app.get('/puppies/new', (req,res) => {
+  res.render('puppies/new.ejs');
+});
+
+//GET ALL puppies list
 app.get('/puppies', async (req,res) => {
   const allPuppies = await Puppy.find();
   console.log(allPuppies);
   res.render('puppies/index.ejs', {puppies: allPuppies});
 })
 
+app.get('/puppies/:puppyId', async (req, res) => {
+  const foundPuppy = await Puppy.findById(req.params.puppyId);
+  console.log(foundPuppy)
+  res.render('puppies/show.ejs', { puppy: foundPuppy});
+})
 
-// GET Puppies New
-app.get('/puppies/new', (req,res) => {
-  res.render('puppies/new.ejs');
-});
-
-//Post puppies/new
+//POST puppies/new
 app.post('/puppies', async (req,res) => {
   if (req.body.isGoodWithChildren === "on") {
     req.body.isGoodWithChildren = true;
@@ -47,6 +57,8 @@ app.post('/puppies', async (req,res) => {
   await Puppy.create(req.body);
   res.redirect('/puppies/new')
 })
+
+//DELETE
 
 
 
